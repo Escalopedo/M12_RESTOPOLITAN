fef728001f6953dca1de38fa229fabfc240484f9
DavidHompaneraCampos <davidhompaneracampos@gmail.com>
2025-02-14 18:07:45 +0100

    filtrosAJAX2

--- Restopolitan/public/js/search.js
+++ Restopolitan/public/js/search.js
@@ -1,3 +1,8 @@
+/*1El usuario escribe en el input de búsqueda (home.blade.php).
+2️AJAX (search.js) envía la petición a Laravel (/search?query=texto).
+3️Laravel (web.php) recibe la petición y consulta la base de datos (Restaurant + Locations).
+4️Laravel renderiza la vista parcial con los resultados (partials/restaurants.blade.php).
+5️AJAX recibe la respuesta y actualiza dinámicamente la lista de restaurantes en home.blade.php.*/
 document.addEventListener("DOMContentLoaded", function () {
     let searchInput = document.getElementById("search-input");
     let searchForm = document.getElementById("search-form");
