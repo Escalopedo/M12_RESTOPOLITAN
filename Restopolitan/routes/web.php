90a117091308e0c36eb560005e4bcd3f98ad79a6
DavidHompaneraCampos <davidhompaneracampos@gmail.com>
2025-02-18 18:23:28 +0100

    merge

--- Restopolitan/routes/web.php
+++ Restopolitan/routes/web.php
@@ -1,16 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\DB; //Nos permite ejecutar consultas SQL más avanzadas, como cálculos con AVG().
+use Illuminate\Support\Facades\DB; 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\RestaurantController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CuisineTypeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Restaurant;
+use App\Models\CuisineType;
 
 // Ruta para la página de inicio
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -28,26 +30,8 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-/*Route::get('/search', function (Request $request) {
-// Ruta para la búsqueda de restaurantes con AJAX
-Route::get('/search', function (Request $request) {
-    $query = $request->input('query');
-
-    $restaurants = Restaurant::join('locations', 'restaurants.location_id', '=', 'locations.id')
-        ->where('restaurants.name', 'like', "%$query%")
-        ->orWhere('locations.street_address', 'like', "%$query%")
-        ->orWhere('locations.city', 'like', "%$query%")
-        ->select('restaurants.*')
-        ->get();
-
-/*$restaurants = Restaurant::join('cuisine_types', 'restaurants.cuisine_type_id', '=', 'cuisine_types.id')
-    ->where('restaurants.name', 'like', "%$query%")
-    ->orWhere('cuisine_types.name', 'like', "%$query%")
-    ->select('restaurants.*') //Muestra los datos de la tabla restaurants
-    ->get();*/
-    return view('partials.restaurants', compact('restaurants'));
-})->name('restaurants.search');
+Route::get('/search', [RestaurantController::class, 'search'])->name('restaurants.search');
+Route::resource('cuisine_types', CuisineTypeController::class);
 
 // Ruta protegida para administradores (Página de Admin)
 Route::middleware(['auth'])->group(function () {
