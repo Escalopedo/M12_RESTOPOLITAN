a9874d75ccfbcab19adf5d9f43876729f129f6c4
PC-ERIC <eric.alcazarc@gmail.com>
2025-02-18 21:40:27 +0100

    Eliminar user con js+

--- Restopolitan/resources/views/admin.blade.php
+++ Restopolitan/resources/views/admin.blade.php
@@ -13,6 +13,8 @@
 
     <script src="{{ asset('js/addUser.js') }}" defer></script>
     <script src="{{ asset('js/editUser.js') }}" defer></script>
+    <script src="{{ asset('js/deleteUser.js') }}" defer></script>
+
 
 
 
@@ -228,48 +230,5 @@
             </form>
         </div>
     </div>
-
-    <script>
-        
-        // RESTAURANTES
-
-        document.addEventListener("DOMContentLoaded", function () {
-
-            // Función para eliminar un usuario
-            document.querySelectorAll('.delete-user').forEach(button => {
-                button.addEventListener('click', function () {
-                    const userId = this.dataset.id;
-                    Swal.fire({
-                        title: "¿Quieres eliminar este usuario?",
-                        text: "¡No podrás revertir esta acción!",
-                        icon: "warning",
-                        showCancelButton: true,
-                        confirmButtonColor: "#d33",
-                        cancelButtonColor: "#3085d6",
-                        confirmButtonText: "Sí, eliminar",
-                        cancelButtonText: "Cancelar"
-                    }).then((result) => {
-                        if (result.isConfirmed) {
-                            fetch(`/users/${userId}`, {
-                                method: 'DELETE',
-                                headers: {
-                                    'Content-Type': 'application/json',
-                                    'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-                                }
-                            })
-                            .then(response => response.json())
-                            .then(data => {
-                                if (data.success) {
-                                    Swal.fire('Eliminado', 'El usuario ha sido eliminado', 'success');
-                                    document.getElementById(`user-${userId}`).remove();
-                                }
-                            });
-                        }
-                    });
-                });
-            });
- 
-        });
-    </script>
 </body>
 </html>
